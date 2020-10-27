38dd9df6f83a02eac07234461419bfe5b3d52e3c
김소정 <sojeong7524@gmail.com>
2020-10-28 04:09:29 +0900

    input button delete button

--- app/src/main/java/com/example/bst/MainActivity.java
+++ app/src/main/java/com/example/bst/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bst;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText inputNumber = (EditText)findViewById(R.id.inputNumber);
+        final Button pushButton = (Button)findViewById(R.id.pushNumber);
+        final Button deleteButton = (Button)findViewById(R.id.deleteNumber);
+
+        final Button root = (Button)findViewById(R.id.root);
+
+        pushButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                root.setText(inputNumber.getText());
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                root.setText("");
+            }
+        });
     }
 }
